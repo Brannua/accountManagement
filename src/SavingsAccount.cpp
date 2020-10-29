38fc6acefece0b21729ae2c2ef25306001a301d3
Brannua <374453156@qq.com>
2020-10-29 14:05:21 +0800

    :construction: 更新主函数测试代码

--- src/SavingsAccount.cpp
+++ src/SavingsAccount.cpp
@@ -5,26 +5,26 @@ const double RATE = 0.015;
 
 int main()
 {
-	// 新建日期，开俩账户
-	Date d1(2020, 10, 29), d2(2020, 10, 30), d3(2020, 11, 7), d4(2020, 11, 17), d5(2020, 11, 19);
+	Date date(2008, 11, 1);
 	SavingsAccount accounts[] = {
-		SavingsAccount("21325302", RATE, d1),
-		SavingsAccount("58320212", RATE, d1)};
+		SavingsAccount("S3755217", 0.015, date),
+		SavingsAccount("02342342", 0.015, date)
+	};
 
-	// 对俩账户一顿操作
-	accounts[0].deposit(d2, 5000, "存款");
-	accounts[1].deposit(d3, 10000, "存款");
-	accounts[1].withDraw(d4, 400, "取款");
-	accounts[0].settle(d5);
-	accounts[1].settle(d5);
+	accounts[0].deposit(Date(2008, 11, 5), 5000, "salary");
+	accounts[1].deposit(Date(2008, 11, 25), 10000, "sell stock 0323");
+	accounts[0].deposit(Date(2008, 12, 5), 5500, "salary");
+	accounts[1].withDraw(Date(2008, 12, 20), 4000, "buy a laptop");
 
-	// 显示俩账户的基本信息
-	for (int i = 0, len = sizeof(accounts) / sizeof(accounts[0]); i < len; i++)
-	{
+	cout << endl;
+
+	for (int i = 0, n = sizeof(accounts) / sizeof(SavingsAccount); i < n; i ++) {
+		accounts[i].settle(Date(2009, 1, 1));
 		accounts[i].show();
+		cout << endl;
 	}
 
-	// 显示户主总资产
-	cout << "所有账户总余额: " << SavingsAccount::getTotal() << endl;
+	cout << "Total: " << SavingsAccount::getTotal() << endl;
+
 	return 0;
 }
